857ee4ff09cde9a1a3b6a56e6396b96f150d072f
Harsh Singh <harshsin@usc.edu>
2021-04-19 00:21:09 -0700

    Add initial state

---
+++ src/main/java/spring5jokesappv2/Spring5jokesappv2Application.java
@@ -0,0 +1,13 @@
+package spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5jokesappv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5jokesappv2Application.class, args);
+    }
+
+}
